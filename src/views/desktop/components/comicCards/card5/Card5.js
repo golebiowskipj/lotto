fddf4f08f171b0a57c83dad4ade99fba0ca4cf96
Piotr <piotr.golebiowski@efirst.asia>
2019-12-05 00:12:02 +0100

    empty cards on desktop ready

---
+++ src/views/desktop/components/comicCards/card5/Card5.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import styles from './Card5.module.scss';
+import '../CommonComicCard.scss';
+
+export const Card5 = (props) => {
+    return (
+        <div style={{ height: props.data.height }} className={`gradientBorder`}>
+            <div className={`card`}>
+                <img style={{ height: props.data.height - 6 }} src={props.data.background} alt="tło karty" />
+            </div>
+        </div>
+    )
+}
+
+Card5.propTypes = {
+    data: PropTypes.shape(
+        {
+            stage: PropTypes.number.isRequired,
+            height: PropTypes.number.isRequired,
+            background: PropTypes.string.isRequired,
+            actors: PropTypes.arrayOf(PropTypes.shape({
+                id: PropTypes.number.isRequired,
+                name: PropTypes.string.isRequired
+            })),
+            dialogues: PropTypes.arrayOf(PropTypes.shape({
+                id: PropTypes.number.isRequired,
+                actorId: PropTypes.number.isRequired,
+                cloudImg: PropTypes.string.isRequired,
+                cloudText: PropTypes.string.isRequired
+            }))
+        }
+    )
+}
+
--- src/views/desktop/components/main/Main.js
+++ src/views/desktop/components/main/Main.js
@@ -1,48 +1,28 @@
 import React from 'react'
+import styles from './Main.module.scss';
+import { comicCards } from '../../../../services/comicCardsFactory/ComicCardsFactory';
+import {Card1} from '../comicCards/card1/Card1';
+import {Card2} from '../comicCards/card2/Card2';
+import {Card3} from '../comicCards/card3/Card3';
+import {Card4} from '../comicCards/card4/Card4';
+import {Card5} from '../comicCards/card5/Card5';
+import {Card6} from '../comicCards/card6/Card6';
 
 export const Main = () => {
     return (
-        <main>
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            <p className="g-text--desktop">hello from main dax</p> 
-            
+        <main className={styles.mainGrid}>
+            <div className={styles.gradient__toRight}></div>
+            <section className={styles.track}>
+                <Card1 data={comicCards[0]} />
+                <Card2 data={comicCards[1]} />
+                <Card3 data={comicCards[2]} />
+                <Card4 data={comicCards[3]} />
+                <Card5 data={comicCards[4]} />
+                <Card6 data={comicCards[5]} />
+            </section>
+            <div className={styles.gradient__toLeft}></div>
         </main>
     )
 }
+
+
