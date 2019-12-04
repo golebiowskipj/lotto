f18b3abbb7925012ae5203f946dba12311bedecd
Piotr <piotr.golebiowski@efirst.asia>
2019-12-04 21:18:03 +0100

    Updates

---
+++ precache-manifest.dcbc99788564897a37cf89830305bd77.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3689819a49f1f1e1ddc863c6a5a2cf3",
+    "url": "/lotto/index.html"
+  },
+  {
+    "revision": "6ce6c66915c88da2b7fd",
+    "url": "/lotto/static/css/main.c0e85ba9.chunk.css"
+  },
+  {
+    "revision": "783b1223c0ee7ce88980",
+    "url": "/lotto/static/js/2.efb7ee6a.chunk.js"
+  },
+  {
+    "revision": "6ce6c66915c88da2b7fd",
+    "url": "/lotto/static/js/main.f65caf7f.chunk.js"
+  },
+  {
+    "revision": "a46015652e4e4d53abbe",
+    "url": "/lotto/static/js/runtime-main.91c2af38.js"
+  },
+  {
+    "revision": "51d6cb1f264ddb398587dd549beb89c5",
+    "url": "/lotto/static/media/DaxBold.51d6cb1f.ttf"
+  },
+  {
+    "revision": "b100b6fb1916257b75f8ec537bcdbece",
+    "url": "/lotto/static/media/DaxRegular.b100b6fb.ttf"
+  },
+  {
+    "revision": "caa6c267977c8452aeaaeb0c073ed59f",
+    "url": "/lotto/static/media/logo-desktop.caa6c267.svg"
+  },
+  {
+    "revision": "126371c4672749bba7e0f3a18729e2c6",
+    "url": "/lotto/static/media/logo-mobile.126371c4.svg"
+  }
+]);
