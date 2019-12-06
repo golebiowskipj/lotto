ca3371c224427b064a2a3b0984d81b9607888792
Piotr <piotr.golebiowski@efirst.asia>
2019-12-06 11:58:48 +0100

    link fix

--- src/views/desktop/components/footer/Footer.js
+++ src/views/desktop/components/footer/Footer.js
@@ -4,7 +4,7 @@ import styles from './Footer.module.scss';
 export const Footer = () => {
     return (
         <footer className={styles.footer}>
-        <a className={`f-text-button--desktop ${styles.footer__button}`} href="https://lotto.pl">Rejestruję się i GRAM</a>
+        <a className={`f-text-button--desktop ${styles.footer__button}`} href="https://gry.lotto.pl/pl/registration.html">Rejestruję się i GRAM</a>
         </footer>
     )
 }
--- src/views/mobile/components/footer/FooterM.js
+++ src/views/mobile/components/footer/FooterM.js
@@ -4,7 +4,7 @@ import styles from './FooterM.module.scss';
 export const FooterM = () => {
     return (
         <footer className={styles.footer}>
-            <a className={`f-text-button--mobile ${styles.footer__button}`} href="https://lotto.pl">Rejestruję się i GRAM</a>
+            <a className={`f-text-button--mobile ${styles.footer__button}`} href="https://gry.lotto.pl/pl/registration.html">Rejestruję się i GRAM</a>
         </footer>
     )
 }
