7eee5a65c49d3f506d694d69473db7afd563a62f
Piotr <piotr.golebiowski@efirst.asia>
2019-12-06 03:47:36 +0100

    card2 mobile

--- src/views/mobile/components/comicCards/card1/Card1.js
+++ src/views/mobile/components/comicCards/card1/Card1.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-
 import styles from './Card1.module.scss';
 import '../CommonComicCardM.scss';
+import { showCloud } from '../../../../../services/animationService/AnimationService';
 
 import kierownik from '../../../images/kierownik.svg';
 import marzycielka from '../../../images/marzycielka.svg';
@@ -16,25 +16,31 @@ import cloud4 from "./images/card1cloud4m.svg";
 
 export class Card1 extends Component {
     state = {
-        isVisible: true
+        isVisible: false,
     }
+
+    componentDidMount = () => {
+        setTimeout(() => {
+            this.setState({ isVisible: true });
+        }, 500);
+    }
+
     render() {
         return (
-
             <div style={{ height: this.props.height }} className={`gradientBorder--mobile`}>
                 <div className={`card--mobile`}>
                     <img style={{ maxHeight: this.props.height - 6 }} src={bg} alt="tło karty" />
 
-                    <div className={`actor--mobile actor__kierownik--mobile ${styles.kierownik}`}>
+                    <div onTransitionEnd={e => showCloud(e, '.js-m-card1-actor2')} style={{ transform: this.state.isVisible ? 'none' : 'translateX(500px)' }} className={`actor--mobile actor__kierownik--mobile ${styles.kierownik}`}>
                         <img src={kierownik} alt="kierownik" />
 
-                        <div className={`cloud--mobile ${styles.cloud_kierownik}`}>
+                        <div className={`js-cloud cloud--mobile ${styles.cloud_kierownik}`}>
                             <img src={cloud1} alt="dymek kierownika" />
                             <p className={`f-text--mobile text--mobile ${styles.text_kierownik}`}>Za chwilę wejdziemy do świata kul… kultowych gier LOTTO.</p>
                         </div>
 
 
-                        <div className={`cloud--mobile ${styles.cloud_kierownik2}`}>
+                        <div className={`js-m-card1-cloud2 cloud--mobile ${styles.cloud_kierownik2}`}>
                             <img src={cloud4} alt="dymek kierownika" />
                             <p className={`f-text--mobile text--mobile ${styles.text_kierownik}`}>Zarejestrujmy się i zakręćmy po kody promocyjne!</p>
                         </div>
@@ -42,20 +48,20 @@ export class Card1 extends Component {
                     </div>
 
 
-                    <div className={`actor--mobile actor__zartownis--mobile ${styles.zartownis}`}>
+                    <div onTransitionEnd={e => showCloud(e, '.js-m-card1-actor3')} className={`js-m-card1-actor2 actor--mobile actor__zartownis--mobile ${styles.zartownis}`}>
                         <img className={``} src={zartownis} alt="zartownis" />
 
-                        <div className={`cloud--mobile ${styles.cloud_zartownis}`}>
+                        <div className={`js-cloud cloud--mobile ${styles.cloud_zartownis}`}>
                             <img src={cloud2} alt="dymek zartownisia" />
                             <p className={`f-text--mobile text--mobile ${styles.text_zartownis}`}>I to mnie kręci!</p>
                         </div>
 
                     </div>
 
-                    <div className={`actor--mobile actor__marzycielka--mobile ${styles.marzycielka}`}>
+                    <div onTransitionEnd={e => showCloud(e, '', '.js-m-card1-cloud2')} className={`js-m-card1-actor3 actor--mobile actor__marzycielka--mobile ${styles.marzycielka}`}>
                         <img className={``} src={marzycielka} alt="marzycielka" />
 
-                        <div className={`cloud--mobile ${styles.cloud_marzycielka}`}>
+                        <div className={`js-cloud cloud--mobile ${styles.cloud_marzycielka}`}>
                             <img src={cloud3} alt="dymek marzycielki" />
                             <p className={`f-text--mobile text--mobile ${styles.text_marzycielka}`}>A mnie rozmarza…</p>
                         </div>
@@ -64,7 +70,6 @@ export class Card1 extends Component {
 
                 </div>
             </div>
-
         )
     }
 
--- src/views/mobile/components/comicCards/card2/Card2.js
+++ src/views/mobile/components/comicCards/card2/Card2.js
@@ -3,35 +3,75 @@ import PropTypes from 'prop-types';
 import styles from './Card2.module.scss';
 import '../CommonComicCardM.scss';
 
-export const Card2 = (props) => {
-    return (
-        <div style={{ height: props.height }} className={`gradientBorder--mobile`}>
-            <div className={`card--mobile`}>
-                <img style={{maxHeight: props.height - 6}} src={props.data.background} alt="tło karty" />
+import { showCloud } from '../../../../../services/animationService/AnimationService';
+
+import kierownik from '../../../images/kierownik.svg';
+import marzycielka from '../../../images/marzycielka.svg';
+import zartownis from '../../../images/zartownis.svg';
+import bg from "./images/card2bgm.jpg"
+import cloud1 from "./images/card2cloud1m.svg";
+import cloud2 from "./images/card2cloud2m.svg";
+import cloud3 from "./images/card2cloud3m.svg";
+
+export class Card2 extends React.Component {
+
+    state = {
+        isVisible: false
+    }
+
+    componentDidMount = () => {
+        setTimeout(() => {
+            this.setState({ isVisible: true });
+        }, 500);
+    }
+    render() {
+        return (
+            <div style={{ height: this.props.height }} className={`gradientBorder--mobile`}>
+                <div className={`card--mobile`}>
+                    <img style={{ maxHeight: this.props.height - 6 }} src={bg} alt="tło karty" />
+
+                    <div onTransitionEnd={e => showCloud(e, '.js-m-card1-actor2')} style={{ transform: this.state.isVisible ? 'none' : 'translateX(500px)' }} className={`actor--mobile actor__kierownik--mobile ${styles.kierownik}`}>
+                        <img src={kierownik} alt="kierownik" />
+
+                        <div className={`js-cloud cloud--mobile ${styles.cloud_kierownik}`}>
+                            <img src={cloud1} alt="dymek kierownika" />
+                            <p className={`f-text--mobile text--mobile ${styles.text_kierownik}`}>Teraz zaangażujcie obie półkule mózgowe. Ustalcie hasło i sześciocyfrowy kod PIN.</p>
+                        </div>
+
+                    </div>
+
+                    <div onTransitionEnd={e => showCloud(e, '.js-m-card2-actor3')} className={`js-m-card1-actor2 actor--mobile actor__marzycielka--mobile ${styles.marzycielka}`}>
+                        <img className={``} src={marzycielka} alt="marzycielka" />
+
+                        <div className={`js-cloud cloud--mobile ${styles.cloud_marzycielka}`}>
+                            <img src={cloud2} alt="dymek marzycielki" />
+                            <p className={`f-text--mobile text--mobile ${styles.text_marzycielka}`}>PIN będzie potrzebny w kontakcie z Obsługą Klienta. Zapiszę go w bezpiecznym miejscu. Może znów dodzwonię się do tego miłego pana…</p>
+                        </div>
+
+                    </div>
+
+
+                    <div onTransitionEnd={e => showCloud(e)} className={`js-m-card2-actor3 actor--mobile actor__zartownis--mobile ${styles.zartownis}`}>
+                        <img className={``} src={zartownis} alt="zartownis" />
+
+                        <div className={`js-cloud cloud--mobile ${styles.cloud_zartownis}`}>
+                            <img src={cloud3} alt="dymek zartownisia" />
+                            <p className={`f-text--mobile text--mobile ${styles.text_zartownis}`}>He, he, albo do tej PIN-up girl…</p>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
 }
 
 Card2.propTypes = {
     height: PropTypes.number.isRequired,
-    data: PropTypes.shape(
-        {
-            stage: PropTypes.number.isRequired,
-            height: PropTypes.oneOfType([PropTypes.number.isRequired, PropTypes.string.isRequired]),
-            background: PropTypes.string.isRequired,
-            actors: PropTypes.arrayOf(PropTypes.shape({
-                id: PropTypes.number.isRequired,
-                name: PropTypes.string.isRequired
-            })),
-            dialogues: PropTypes.arrayOf(PropTypes.shape({
-                id: PropTypes.number.isRequired,
-                actorId: PropTypes.number.isRequired,
-                cloudImg: PropTypes.string.isRequired,
-                cloudText: PropTypes.string.isRequired
-            }))
-        }
-    )
 }
 
 
